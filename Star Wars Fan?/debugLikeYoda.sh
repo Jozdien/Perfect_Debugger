5bf0bf9547850865e9414c7cb34d4ae74e9b60db
AbhishekS-2099 <abhishek.sankar.in@gmail.com>
2019-12-16 20:43:35 +0530

    So Anakin has spoken

--- Star Wars Fan?/debugLikeYoda.sh
+++ Star Wars Fan?/debugLikeYoda.sh
@@ -1,6 +1,15 @@
 #/bin/bash
 number=$RANDOM
-echo $number
-
-
+modNumber=$(($number%3))
+if [[ $modNumber -eq 0 ]]
+then
+	echo "May the force be with you">$1
+elif [[ $modNumber -eq 1 ]]
+then
+	echo "No. I am your father.">$1
+else
+	echo "Search your feelings, you know you to be true.">$1
+fi
 echo "Do. Or do not. There is no try."
+#http://www.planetofsuccess.com/blog/2019/star-wars-quotes/
+#for the quotes
